6485b7add38e057fb05f232c3d15fd66199cfc79
Erin Mettille <ecmettille@gmail.com>
2020-04-17 23:54:53 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -33,5 +33,19 @@ def sum_array(array)
 end
 
 def add_s(array)
-  array.each_with_index.collect { |element, index| }
+  array.each_with_index.collect do |element, index| 
+    if element[1]
+      element
+    else
+      element << "s"
+      
+    end
 end
+
+
+
+
+
+
+
+
